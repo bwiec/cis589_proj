362cdcc1af60ed6b3ea1cc1578ef0dbc6fd69b5f
Brian Wiec <bswiec@gmail.com>
2024-03-22 08:22:32 -0400

    Refactor cloud

--- cloud.py
+++ cloud.py
@@ -1,24 +1,31 @@
 #! /usr/bin/env python3
+from common.camera import camera
+import sys
+import argparse
 import cv2
 import boto3
 import time
 import io
 from PIL import Image, ImageDraw
 
-def detect_labels_local_file(photo):
-    client = boto3.client('rekognition')
-    
-    with open(photo, 'rb') as image:
+def process(frame, algorithm):
+    print('Processing frame')
+    duration = 0
+    if algorithm == 'detect_faces':
         start = time.time()
-        response = client.detect_labels(Image={'Bytes': image.read()})
+        faces_count = show_faces(frame)
         end = time.time()
-        print('inference time: ' + str(end-start))
-        
-    print('Detected labels in ' + photo)
-    for label in response['Labels']:
-        print(label['Name'] + ' : ' + str(label['Confidence']))
-        
-    return len(response['Labels'])
+        duration = end - start
+        print("Faces detected: " + str(faces_count))
+
+    if algorithm == 'detect_labels':
+        start = time.time()
+        label_count = detect_labels_local_file(frame)
+        end = time.time()
+        duration = end - start
+        print("Labels detected: " + str(label_count))
+
+    return duration
 
 def show_faces(photo):
     client = boto3.client('rekognition')
@@ -59,27 +66,54 @@ def show_faces(photo):
     image.show()
     return len(response['FaceDetails'])
 
-
-label_count = detect_labels_local_file('labels_sample.jpg')
-print("Labels detected: " + str(label_count))
-
-faces_count = show_faces('faces_sample.jpg')
-print("Faces detected: " + str(faces_count))
-exit()
-
-cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-if not cap.isOpened():
-    print("Cannot open camera")
-    exit()
-
-while True:
-    ret, frame = cap.read()
+def detect_labels_local_file(photo):
+    client = boto3.client('rekognition')
     
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(1) == ord('q'):
-        break
+    with open(photo, 'rb') as image:
+        start = time.time()
+        response = client.detect_labels(Image={'Bytes': image.read()})
+        end = time.time()
+        print('inference time: ' + str(end-start))
+        
+    print('Detected labels in ' + photo)
+    for label in response['Labels']:
+        print(label['Name'] + ' : ' + str(label['Confidence']))
+        
+    return len(response['Labels'])
 
-cap.release()
-cv2.destroyAllWindows()
+if __name__ == "__main__":
+    arg_parser = argparse.ArgumentParser(prog=sys.argv[0], description='Edge-based ML implementation on AMD/Xilinx KV260')
+    arg_parser.add_argument('--print_duration', action='store_true', help='Print processing duration to console')
+    arg_parser.add_argument('--display', action='store_true', help='Display resulting image')
+    arg_parser.add_argument('--test_image', action='store', nargs=1, help='Test with an image (i.e. .jpg) instead of camera stream')
+    arg_parser.add_argument('--algorithm', action='store', default='detect_faces', nargs=1, help='Choose which algorithm to run (detect_faces, detect_labels)')
+    Args = sys.argv
+    Args.pop(0)
+    args = arg_parser.parse_args(Args)
+
+    if args.algorithm != 'detect_faces' or args.algorithm != 'detect_labels':
+        raise Exception("Unsupported algorithm " + args.algorithm)
+    
+    if args.test_image != '':
+        frame = args.test_image
+        process_duration = process(frame, args.algorithm)
+        if args.print_duration:
+            print('process_duration: ' + str(process_duration))
+        if args.display:
+            cv2.imshow('Test image', frame)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+        exit()
+
+    cam = camera()
+
+    while True:
+        frame, capture_duration = cam.read()
+        process_duration = process(frame, args.algorithm)
+        if args.print_duration:
+            print('capture_duration: ' + str(capture_duration))
+            print('process_duration: ' + str(process_duration))
+        if args.display:
+            cam.display(frame)
+            if cv2.waitKey(1) == ord('q'):
+                break
